6f8cb320669b67cb86bc4b4cf15932783c0762c9
phagoth <olexandr.averyanov@gmail.com>
2015-07-01 02:40:45 +0300

    add some tests

--- library_manager_spec.rb
+++ library_manager_spec.rb
@@ -2,16 +2,20 @@ require './library_manager.rb'
 
 describe LibraryManager do
 
-  let(:leo_tolstoy) do
-    Author.new(1828, 1910, 'Leo Tolstoy' )
-  end
+  let(:leo_tolstoy) { Author.new(1828, 1910, 'Leo Tolstoy' ) }
   let(:oscar_wilde) { Author.new(1854, 1900, 'Oscar Wilde') }
   let(:war_and_peace) { PublishedBook.new(leo_tolstoy, 'War and Peace', 1400, 3280, 1996) }
+  let(:dorian_gray) { PublishedBook.new(oscar_wilde, 'The Picture of Dorian Gray', 580, 192, 2004) }
   let(:ivan_testenko) { ReaderWithBook.new('Ivan Testenko', 16, war_and_peace, 328) }
+  let(:mark_testenko) { ReaderWithBook.new('Mark Testenko', 12, dorian_gray, 48) }
   let(:manager) { LibraryManager.new(ivan_testenko, (DateTime.now.new_offset(0) - 2.days)) }
+  let(:manager2) { LibraryManager.new(ivan_testenko, (DateTime.now.new_offset(0) - 100.hours)) }
+  let(:manager3) { LibraryManager.new(mark_testenko, (DateTime.now.new_offset(0) - 3.days)) }
+  let(:manager4) { LibraryManager.new(mark_testenko, (DateTime.now.new_offset(0) - 200.hours)) }
 
   it 'should count penalty' do
-    manager.penalty
+    res = manager.penalty
+    expect(res).to eq 784
   end
 
   it 'should know if author can meet another author' do
