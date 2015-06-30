84cfc29da8e934094bdcdf5e55f59e650b6b63b7
Petro Koriakin <pkor@softserveinc.com>
2015-06-30 21:21:49 +0300

    requires done right

--- library_manager_spec.rb
+++ library_manager_spec.rb
@@ -1,13 +1,3 @@
-require 'active_support/all'
-require 'pry'
-
-require './library_manager.rb'
-require './author.rb'
-require './book.rb'
-require './published_book.rb'
-require './reader.rb'
-require './reader_with_book.rb'
-
 describe LibraryManager do
 
   let(:leo_tolstoy) do
